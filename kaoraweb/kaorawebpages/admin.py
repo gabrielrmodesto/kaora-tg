e3a931a9b577ca786fa6727f463476008e020de7
GabrielRM <gabrielrmodesto5@gmail.com>
2018-10-25 08:15:34 -0300

    Administrativo para o projeto, principalmente testes em relação ao bd

--- kaoraweb/kaorawebpages/admin.py
+++ kaoraweb/kaorawebpages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Fisioterapeuta
+from .models import Paciente
+from .models import Login
 
-# Register your models here.
+admin.site.register(Fisioterapeuta)
+admin.site.register(Paciente)
+admin.site.register(Login)
