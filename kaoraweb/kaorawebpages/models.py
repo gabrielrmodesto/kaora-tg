78f8a57fc6e5021e1db31255ab51210bb379275e
GabrielRM <gabrielrmodesto5@gmail.com>
2018-10-25 08:09:26 -0300

    Models do projeto para o bd

--- kaoraweb/kaorawebpages/models.py
+++ kaoraweb/kaorawebpages/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+class Login(models.Model):
+    email = models.EmailField(max_length=100)
+    senha = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
+
+class Fisioterapeuta(models.Model):
+    nome = models.CharField(max_length=100)
+    cpfFisioterapeuta = models.CharField(max_length=20)
+    crefito = models.CharField(max_length=15)
+    especialidade = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=100)
+    senha = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Paciente(models.Model):
+    nome = models.CharField(max_length=100)
+    cpfPaciente = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=200)
+    bairro = models.CharField(max_length=40)
+    cep = models.CharField(max_length=15)
+    cidade = models.CharField(max_length=40)
+    uf = models.CharField(max_length=2, blank=True, null=True)
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+    celular = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    dataNascimento = models.DateField()
+    responsavel = models.CharField(max_length=100, blank=True, null=True)
+    cpfResponsavel = models.CharField(max_length=20, blank=True, null=True)
+    diagnostico = models.CharField(max_length=200)
+    descricaoDiagnostico = models.TextField()
+    fotos = models.ImageField(upload_to='static/img/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+class Anotacao_Paciente(models.Model):
+    data = models.DateField()
+    anotacao = models.TextField()
+    parteCorpo = models.CharField(max_length=40, blank=True, null=True)
+
+    def __str__(self):
+        return self.anotacao
+
+class Dados_Musculos(models.Model):
+    dados_musculos = models.IntegerField()
+    data = models.DateTimeField()
+
+    def __str__(self):
+        return self.data
