76c3a6473e45575af82f24bcb90280317306bc08
GabrielRM <gabrielrmodesto5@gmail.com>
2018-10-25 08:11:35 -0300

    Urls para páginas dos templates do projeto

--- kaoraweb/kaorawebprojeto/urls.py
+++ kaoraweb/kaorawebprojeto/urls.py
@@ -1,21 +1,16 @@
-"""kaorawebprojeto URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from kaorawebpages.views import Cadastro_Fisioterapeuta, Cadastro_Paciente, logar, home, Consulta_Paciente, Perfil_Paciente, sair, Anotacao, Avaliacao
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', logar, name='login'),
+    path('cadastro/', Cadastro_Fisioterapeuta, name='cadastro_fisioterapeuta'),
+    path('home/', home, name='pagina_inicial'),
+    path('cadastro_paciente/', Cadastro_Paciente, name='cadastro_paciente'),
+    path('consulta_paciente/', Consulta_Paciente, name='consulta_paciente'),
+    path('perfil_paciente/<int:pk>/', Perfil_Paciente, name='perfil_paciente'),
+    path('logout/', sair, name='logout'),
+    path('anotacao/', Anotacao, name='anotacao'),
+    path('avaliacao/', Avaliacao, name='avaliacao'),
 ]
