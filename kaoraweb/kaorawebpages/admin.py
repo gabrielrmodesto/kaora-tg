4583b5155db834d0ceb896d817251c3751120d85
GabrielRM <gabrielrmodesto5@gmail.com>
2018-10-28 17:47:04 -0300

    Administrativo do projeto

--- kaoraweb/kaorawebpages/admin.py
+++ kaoraweb/kaorawebpages/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Fisioterapeuta
 from .models import Paciente
 from .models import Login
+from .models import Dados_Musculos
 
 admin.site.register(Fisioterapeuta)
 admin.site.register(Paciente)
 admin.site.register(Login)
+admin.site.register(Dados_Musculos)
