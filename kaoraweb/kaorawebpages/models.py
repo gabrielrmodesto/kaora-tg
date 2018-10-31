cb0a0da5e19591e8b1a120c995206c8e2e967550
GabrielRM <gabrielrmodesto5@gmail.com>
2018-10-31 19:40:51 -0300

    Tentativa de choicefield

--- kaoraweb/kaorawebpages/models.py
+++ kaoraweb/kaorawebpages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+
 class Login(models.Model):
     email = models.EmailField(max_length=100)
     senha = models.CharField(max_length=100)
