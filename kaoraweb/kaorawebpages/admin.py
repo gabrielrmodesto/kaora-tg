7c74d355d51e2995142ed04844ba3189e0a05f1c
GabrielRM <gabrielrmodesto5@gmail.com>
2018-11-01 08:18:20 -0300

    rota para anotacao de paciente

--- kaoraweb/kaorawebpages/admin.py
+++ kaoraweb/kaorawebpages/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from .models import Fisioterapeuta
-from .models import Paciente
-from .models import Login
-from .models import Dados_Musculos
+from .models import Fisioterapeuta, Paciente, Login, Dados_Musculos, Anotacao_Paciente
 
 admin.site.register(Fisioterapeuta)
 admin.site.register(Paciente)
 admin.site.register(Login)
 admin.site.register(Dados_Musculos)
+admin.site.register(Anotacao_Paciente)
