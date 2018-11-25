13ab2073c6da71f2809b52491924479748421533
GabrielRM <gabrielrmodesto5@gmail.com>
2018-11-25 17:51:27 -0200

    Implementação parte arduino

--- kaoraarduino/Kaora/Kaora.ino
+++ kaoraarduino/Kaora/Kaora.ino
@@ -1,7 +1,9 @@
-#define velocidade 115200
-#define porta A3
+#define velocidade 9600
+#define porta A2
+#define ID '1' // identification
 
 int dado_musculo;
+int calibragem[10];
 
 void setup() {
   // Definindo a velocidade do emg
@@ -11,8 +13,32 @@ void setup() {
 void loop() {
   // variavel que ira receber o valor analisado pelo emg
   int dado_musculo = analogRead(porta);
+  for(int i = 0; i < 10; i++){
+    calibragem[i] = dado_musculo;
+  }
   //printando o valor da variavel
   Serial.println(dado_musculo);
   // imprimindo a cada 1 seg
-  delay(1000);
+  delay(500);
+}
+
+void serialEvent() // routine of serial event
+{
+  char receive = Serial.read();
+  
+  if(receive == ID) // was received the ID
+  {
+    if(isnan(dado_musculo)) // check error of read 
+    {
+      Serial.println("{\"error\": \"Falha ao ler o sensor\"}");
+    }
+    else
+    {
+      Serial.print("{");
+      Serial.print("\"Dados Musculares\": ");
+      Serial.print(dado_musculo);
+      Serial.print("}");
+      Serial.println(); // treat to receive string /r/n. 
+    }
+  }
 }
